af542362e6f411eaf9217a08176bda66cb5638ea
ɐllɐ <molchanova.a.a@gmail.com>
2024-11-27 22:34:54 +0300

    task 15.1.6

--- SkillFactoryCSharp15LINQ/Program.cs
+++ SkillFactoryCSharp15LINQ/Program.cs
@@ -5,22 +5,27 @@ namespace SkillFactoryCSharp15LINQ
 {
     class Program
     {
+        /// <summary>
+        /// список содержащихся в строке уникальных символов
+        /// без пробелов и следующих знаков препинания: , . ; :  ? !.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            var softwareManufacturers = new List<string>()
+            var punctuation = new List<char>()
             {
-                "Microsoft", "Apple", "Oracle"
+                ' ', ',', '.', ';', ':', '?', '!'
             };
 
-            var hardwareManufacturers = new List<string>()
-            {
-                "Apple", "Samsung", "Intel"
-            };
+            Console.WriteLine("Please enter the text: ");
+            var text = Console.ReadLine();
 
-            var itCompanies = softwareManufacturers.Union(hardwareManufacturers);
-            foreach (var item in itCompanies)
+            var result = text.Except(punctuation).ToArray();
+            Console.WriteLine("Punctuation-free text: \n");
+
+            foreach (var item in result)
             {
-                Console.Write(item + " ");
+                Console.Write(item);
             }
             Console.ReadKey();
         }
