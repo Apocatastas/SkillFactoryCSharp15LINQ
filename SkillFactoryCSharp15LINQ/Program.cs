981eb92d6d6940166dd9c83f1ae0c3d44a7cbe62
ɐllɐ <molchanova.a.a@gmail.com>
2024-11-27 22:04:06 +0300

    task 15.1.4

--- SkillFactoryCSharp15LINQ/Program.cs
+++ SkillFactoryCSharp15LINQ/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+using System.Linq;
+
+namespace SkillFactoryCSharp15LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first word: ");
+            string word1 = Console.ReadLine();
+            Console.WriteLine("\nPlease enter the second word: ");
+            string word2 = Console.ReadLine();
+
+            Console.WriteLine(Intersector(word1, word2));
+            Console.ReadKey();
+        }
+
+        static int Intersector(string word1, string word2)
+        {
+            var isect = word1.Intersect(word2);
+            Console.WriteLine("\nIntersection: ");
+            foreach (var item in isect)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\nIntersection count: ");
+            return isect.Count();
+        }
+    }
+}
