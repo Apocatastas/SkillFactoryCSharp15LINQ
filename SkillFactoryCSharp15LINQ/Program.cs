b27c96e101ba6d259351a5f8cb4f748c1703ecc4
ɐllɐ <molchanova.a.a@gmail.com>
2024-11-27 22:21:41 +0300

    task 15.1.5

--- SkillFactoryCSharp15LINQ/Program.cs
+++ SkillFactoryCSharp15LINQ/Program.cs
@@ -7,25 +7,22 @@ namespace SkillFactoryCSharp15LINQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the first word: ");
-            string word1 = Console.ReadLine();
-            Console.WriteLine("\nPlease enter the second word: ");
-            string word2 = Console.ReadLine();
+            var softwareManufacturers = new List<string>()
+            {
+                "Microsoft", "Apple", "Oracle"
+            };
 
-            Console.WriteLine(Intersector(word1, word2));
-            Console.ReadKey();
-        }
+            var hardwareManufacturers = new List<string>()
+            {
+                "Apple", "Samsung", "Intel"
+            };
 
-        static int Intersector(string word1, string word2)
-        {
-            var isect = word1.Intersect(word2);
-            Console.WriteLine("\nIntersection: ");
-            foreach (var item in isect)
+            var itCompanies = softwareManufacturers.Union(hardwareManufacturers);
+            foreach (var item in itCompanies)
             {
                 Console.Write(item + " ");
             }
-            Console.WriteLine("\nIntersection count: ");
-            return isect.Count();
+            Console.ReadKey();
         }
     }
 }
