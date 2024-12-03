6506ec6c4d58dd63bae0296933aacbc1fbe0f562
ɐllɐ <molchanova.a.a@gmail.com>
2024-12-03 13:54:00 +0300

    task 15.2.2

---
+++ SkillFactoryCSharp15LINQ/Contact.cs
@@ -0,0 +1,8 @@
+﻿namespace SkillFactoryCSharp15LINQ
+{
+    internal class Contact
+    {
+        public string Name { get; internal set; }
+        public long Phone { get; internal set; }
+    }
+}
--- SkillFactoryCSharp15LINQ/Program.cs
+++ SkillFactoryCSharp15LINQ/Program.cs
@@ -7,16 +7,20 @@ namespace SkillFactoryCSharp15LINQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the number: ");
-            int osn = Convert.ToInt32(Console.ReadLine());
-            List<int> ints = new List<int>();
-            for (int i = 1; i <= osn; i++)
+            var contacts = new List<Contact>()
             {
-                ints.Add(i);
-            }
-            double result = ints.Aggregate((x, y) => x*y);
-            Console.WriteLine("\nFactorial {0} = {1}", osn, result);
+               new Contact() { Name = "Андрей", Phone = 72345678901 },
+               new Contact() { Name = "Сергей", Phone = 72345678901 },
+               new Contact() { Name = "Иван", Phone = 79999675334 },
+               new Contact() { Name = "Игорь", Phone = 8884994 },
+               new Contact() { Name = "Анна", Phone = 665565656 },
+               new Contact() { Name = "Василий", Phone = 3434 }
+            };
+
+            int result = contacts.Count(s => (s.Phone.ToString().Length != 11)||!s.Phone.ToString().StartsWith('7'));
+            Console.WriteLine(result);
             Console.ReadKey();
         }
     }
 }
+
