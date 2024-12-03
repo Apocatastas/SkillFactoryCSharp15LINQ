f4d21be1f906d21dded28b8bae34e23069844672
ɐllɐ <molchanova.a.a@gmail.com>
2024-12-03 14:15:37 +0300

    task 15.3.3

---
+++ SkillFactoryCSharp15LINQ/Contact.cs
@@ -0,0 +1,16 @@
+﻿namespace SkillFactoryCSharp15LINQ
+{
+    internal class Contact
+    {
+        public string Name;
+        public long Number;
+        public string Email;
+
+        public Contact(string v1, long v2, string v3)
+        {
+            this.Name = v1;
+            this.Number = v2;
+            this.Email = v3;
+        }
+    }
+}
--- SkillFactoryCSharp15LINQ/Program.cs
+++ SkillFactoryCSharp15LINQ/Program.cs
@@ -9,26 +9,35 @@ namespace SkillFactoryCSharp15LINQ
 
         static void Main(string[] args)
         {
-            while (true)
+            var phoneBook = new List<Contact>();
+            phoneBook.Add(new Contact("Игорь", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", 799900000013, "serg@gmail.com"));
+            phoneBook.Add(new Contact("Иннокентий", 799900000013, "innokentii@example.com"));
+
+
+            var grouped = phoneBook.GroupBy(c => c.Email.Split("@").Last());
+
+            foreach (var group in grouped)
             {
-                var input = Console.ReadLine();
-                var isInteger = Int32.TryParse(input, out int inputNum);
-                if (!isInteger)
+                if (group.Key.Contains("example"))
                 {
-                    Console.WriteLine();
-                    Console.WriteLine("Вы ввели не число");
+                    Console.WriteLine("Фейковые адреса: ");
+
+                    foreach (var contact in group)
+                        Console.WriteLine(contact.Name + " " + contact.Email);
                 }
                 else
                 {
-                    Numbers.Add(inputNum);
-                    Console.WriteLine("Число " + input + " добавлено в список.");
-                    Console.WriteLine($" Всего в списке  {Numbers.Count} чисел");
-                    Console.WriteLine($"Сумма:  {Numbers.Sum()}");
-                    Console.WriteLine($"Наибольшее:  {Numbers.Max()}");
-                    Console.WriteLine($"Наименьшее:  {Numbers.Min()}"); Console.WriteLine($"Среднее:  {Numbers.Average()}");
-                    Console.WriteLine();
+                    Console.WriteLine("Реальные адреса: ");
+                    foreach (var contact in group)
+                    Console.WriteLine(contact.Name + " " + contact.Email);
                 }
+                Console.WriteLine();
             }
+            Console.ReadKey();
         }
     }
 }
